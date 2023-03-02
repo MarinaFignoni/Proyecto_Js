4fd6462168efb47fb767449110d517015867e2f9
Marina Fignoni <marinafignoni@gmail.com>
2023-03-02 20:49:14 -0300

    DOM

--- index.js
+++ index.js
@@ -1,74 +1,106 @@
-alert("Bienvenido a MF Cambio, conversor de Divisas");
-
-// Array de objetos
 const tasasDeCambio = [
-    { moneda: "usd", tasa: 380, simbolo: "$", nombre: "Dólar" },
+    { moneda: "usd", tasa: 380, simbolo: "$", nombre: "Dólar Estadounidense" },
     { moneda: "euro", tasa: 410, simbolo: "€", nombre: "Euro" },
-    { moneda: "real", tasa: 83, simbolo: "R$", nombre: "Real" },
+    { moneda: "real", tasa: 83, simbolo: "R$", nombre: "Real Brasileño" },
+    { moneda: "gbp", tasa: 450, simbolo: "£", nombre: "Libra Esterlina" },
 ];
 
+const monedasAceptadas = ["usd", "euro", "real", "gbp"];
 
-// Array monedas aceptadas
-const monedasAceptadas = ["usd", "euro", "real"];
+const cotizarButton = document.getElementById("cotizar");
+const resultadoDiv = document.getElementById("resultado");
+const errorDiv = document.getElementById("error");
+const historialDiv = document.getElementById("historial");
+const cotizaciones = [];
 
+function guardarDatos() {
+    localStorage.setItem("cotizaciones", JSON.stringify(cotizaciones));
+}
 
-// Función cotizacion
-function cotizacion(monto, moneda) {
+function cargarDatos() {
+    const data = localStorage.getItem("cotizaciones");
+    if (data) {
+        cotizaciones.push(...JSON.parse(data));
+        actualizarHistorial();
+    }
+}
+
+cargarDatos();
+
+cotizarButton.addEventListener("click", () => {
+    const moneda = document.getElementById("moneda").value;
+    const monto = parseInt(document.getElementById("monto").value);
+
+    if (isNaN(monto) || monto <= 0) {
+        errorDiv.innerText = "Monto no válido, el monto debe ser un número positivo";
+        resultadoDiv.innerHTML = "";
+        return;
+    }
+
+    if (!monedasAceptadas.includes(moneda)) {
+        errorDiv.innerText = "Moneda no válida";
+        resultadoDiv.innerHTML = "";
+        return;
+    }
+
     const tasa = tasasDeCambio.find((tasa) => tasa.moneda === moneda);
-    if (tasa) {
-        const total = monto * tasa.tasa;
-        return {
-            simbolo: tasa.simbolo,
-            nombre: tasa.nombre,
-            total: total,
-        };
-    } else {
-        return null;
+
+    if (!tasa) {
+        errorDiv.innerText = "Ha ocurrido un error. Inténtalo de nuevo.";
+        resultadoDiv.innerHTML = "";
+        return;
     }
+
+    const total = monto * tasa.tasa;
+    const cotizacion = {
+        moneda: tasa.nombre,
+        monto: monto,
+        total: total,
+        fecha: new Date()
+    };
+    cotizaciones.push(cotizacion);
+
+    guardarDatos();
+
+    resultadoDiv.innerHTML = `
+      <p>Nueva cotización:</p>
+      <p>Monto a cotizar: ${tasa.simbolo}${monto}</p>
+      <p>Moneda: ${tasa.nombre}</p>
+      <p>Total en pesos: $${total}</p>
+    `;
+
+    errorDiv.innerHTML = "";
+
+    actualizarHistorial();
+});
+
+function actualizarHistorial() {
+    historialDiv.innerHTML = "";
+    cotizaciones.forEach((cotizacion, index) => {
+        const cotizacionDiv = document.createElement("div");
+        cotizacionDiv.innerHTML = `
+        <p>Cotización anterior:</p>
+        <p>Monto cotizado: ${cotizacion.monto} ${cotizacion.moneda}</p>
+        <p>Total en pesos: $${cotizacion.total}</p>
+        <p>Fecha: ${cotizacion.fecha.toLocaleString()}</p>
+      `;
+        const borrarHistorialButton = document.getElementById("borrarHistorial");
+        borrarHistorialButton.addEventListener("click", () => {
+            cotizaciones.length = 0;
+            actualizarHistorial();
+        });
+
+        const limpiarBtn = document.createElement("button");
+        limpiarBtn.innerText = "Limpiar";
+        limpiarBtn.addEventListener("click", () => {
+            cotizaciones.splice(index, 1);
+            guardarDatos();
+            actualizarHistorial();
+        });
+
+        cotizacionDiv.appendChild(limpiarBtn);
+        historialDiv.appendChild(cotizacionDiv);
+    });
 }
-// Bucle principal
-let cotizar = true;
 
-while (cotizar) {
-    // Pedir moneda y monto a cotizar
-    let moneda = prompt(
-        "Elige la moneda que quieres cotizar: usd, euro, real"
-    ).toLowerCase();
-    while (!monedasAceptadas.includes(moneda)) {
-        alert("Moneda no válida");
-        moneda = prompt(
-            "Elige la moneda que quieres cotizar: usd, euro, real"
-        ).toLowerCase();
-    }
-
-    let monto = prompt("Ingresa el monto:");
-    while (isNaN(monto) || monto <= 0) {
-        alert("Monto no válido el monto debe ser un numero positivo");
-        monto = prompt("Ingresa el monto:");
-    }
-    monto = parseInt(monto);
-
-    // Calcular cotización
-    let resultado = cotizacion(monto, moneda);
-
-    if (resultado) {
-        // Mostrar resultado
-        console.log("Nueva cotización:");
-        console.log(`Monto a cotizar: ${resultado.simbolo} ${monto}`);
-        console.log(`Moneda: ${resultado.nombre}`);
-        console.log(`Total en pesos: $${resultado.total}`);
-        console.log("");
-
-
-        // Preguntar si se desea cotizar de nuevo
-        let respuesta = prompt("¿Quieres cotizar de nuevo? si/no").toLowerCase();
-        while (respuesta !== "si" && respuesta !== "no") {
-            alert("Respuesta no válida");
-            respuesta = prompt("¿Quieres cotizar de nuevo? si/no").toLowerCase();
-        }
-        cotizar = respuesta === "si";
-    } 
-}
-
-// Alerta de despedida
-alert("Gracias por usar el Conversor de Divisas");
+actualizarHistorial();
